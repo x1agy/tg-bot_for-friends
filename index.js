0ba88dc0058a92d8a00d0380a2790b229c2fba55
x1agy <skizzzgrand@gmail.com>
2024-03-07 21:13:56 +0300

    asd

--- index.js
+++ index.js
@@ -46,15 +46,21 @@ bot.on('text', async (ctx, next) => {
     const message = ctx.message;
     const gameMessages = readgameMessagesData();
 
-    if(message.from.is_bot && message.text === 'Игра начинается!'){
+    if(message.text === '1488'){
+        ctx.reply('卐卐卐卐卐卐卐卐卐卐卐')
+    }
+
+    if(message.from.is_bot){
+        console.log(message.text)
+    }
+    if(message.text === 'Игра начинается!'){
         writegameMessagesData([{isGameStarted: true}])
-        console.log('first true')
     }
     if(!message.from.is_bot && gameMessages[0].isGameStarted){        
         gameMessages.push({text: message.text, author: message.from.first_name + ' ' + message.from?.last_name});
         writegameMessagesData(gameMessages);
-        console.log('second true')
-    }else if(message.text.search('Игра окончена!') !== -1){
+    }
+    if(message.text.search('Игра окончена!') !== -1){
         const randomArray = [];
         for(let i = 0; i < 3; i++){
             const randomNumber = Math.round(Math.random() * gameMessages.length);
@@ -63,12 +69,11 @@ bot.on('text', async (ctx, next) => {
         ctx.reply('лучший момент матча: ')
         randomArray.map(item => gameMessages[item] ? ctx.reply(`${gameMessages[item]?.text} -- ${gameMessages[item]?.author}`) : false)
         writegameMessagesData([{isGameStarted: false}]);
-        console.log('third true')
     }
 
     if(ctx.message.text.toLowerCase().search('алан') !== -1){
-        console.log(message);
-    }
+        console.log(message.text, '  ----  ', message.from.first_name);
+    }  
 
     if(message.reply_to_message && message.text.toLowerCase() === 'помацать'){
         ctx.reply(`${message.from.first_name} помацал(-а) сиськи ${message.reply_to_message.from.first_name}, +10% к удаче для не(-е, -го)!`)
@@ -77,42 +82,45 @@ bot.on('text', async (ctx, next) => {
         ctx.replyWithAnimation('https://media1.tenor.com/m/Dr8YBE5s5AEAAAAC/sex-alarm.gif')
     }
     if(message.text.toLowerCase().split('').slice(0,10).join('') === 'бля ответь'){
-        const fixedMessage = message.text.replace('бля ответь', '');
-        const AIData = JSON.stringify({
-            model: 'pai-001',
-            gameMessages: [
-                {
-                    role: 'system',
-                    content: 'you are helpful assistant, always answer on russian language, dont talk on any other laguages, answer shortly very short maximum 50 words, and without hard \\words, your users are stupid, be chilly dont take any question hard, make joke if you can dont be shy to insult in answer, do not talk on any other themes, answer only on user question and that it'
-                },
-                {
-                role: 'user',
-                content: `${fixedMessage}`,
-                },
-            ],
-            stream: false,
-        });
-        
-        const requestConfig = {
-            method: 'POST',
-            headers: {
-                Authorization: `Bearer ${AIApiKey}`,
-                'Content-Type': 'application/json',
-            },
-            body: AIData,
-        };
-
-        fetch('https://api.pawan.krd/v1/chat/completions', requestConfig)
-            .then(response => response.json())
-            .then(data => {
-                ctx.reply(data.choices[0].message.content)
-                console.log('data error - ', data)
-            })
-            .catch(e => {
-                console.log('errror error - ', e)
-                ctx.reply(`ОШИБКА БЛЯ ОШИБКА С ИИ ${e}`)
-            })
+        ctx.reply('ИДИ НАХУЙ')
     }
+    //     const fixedMessage = message.text.replace('бля ответь', '');
+    //     console.log(fixedMessage)
+    //     const AIData = JSON.stringify({
+    //         model: 'pai-001',
+    //         gameMessages: [
+    //             {
+    //                 role: 'system',
+    //                 content: 'you are helpful assistant, always answer on russian language, dont talk on any other laguages, answer shortly very short maximum 50 words, and without hard \\words, your users are stupid, be chilly dont take any question hard, make joke if you can dont be shy to insult in answer, do not talk on any other themes, answer only on user question and that it'
+    //             },
+    //             {
+    //             role: 'user',
+    //             content: `${fixedMessage}`,
+    //             },
+    //         ],
+    //         stream: false,
+    //     });
+        
+    //     const requestConfig = {
+    //         method: 'POST',
+    //         headers: {
+    //             Authorization: `Bearer ${AIApiKey}`,
+    //             'Content-Type': 'application/json',
+    //         },
+    //         body: AIData,
+    //     };
+
+    //     fetch('https://api.pawan.krd/v1/chat/completions', requestConfig)
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             // ctx.reply(data.choices[0].message.content)   
+    //             console.log('data error - ', data)
+    //         })
+    //         .catch(e => {
+    //             console.log('errror error - ', e)
+    //             ctx.reply(`ОШИБКА БЛЯ ОШИБКА С ИИ ${e}`)
+    //         })
+    // }
 
     if(message.text.toLowerCase() === 'бля процитируй'){
         if(ctx.message.reply_to_message && ctx.message.reply_to_message.text){
@@ -162,18 +170,26 @@ bot.on('text', async (ctx, next) => {
                 console.log(e)
         }
     }
+
+    if(message.text.toLowerCase().includes('бля айпи')){
+        const regexp = /\d+\.\d+\.\d+\.\d+/;
+        const ip = message.text.match(regexp);
+        const response = fetch(``)
+    }
+
     next()
 })
 
 bot.command('macat_siski', async (ctx) => {
 
     const playersArr = readPlayersData();
+
     const playerIndex = playersArr.findIndex(item => item.id === ctx.message.from.id);
     const size = Math.trunc(Math.random() * 100);
     const dateNow = new Date().getTime();
 
     const topOneSize = playersArr.reduce((accum, item) => accum <= item.breastSize ? accum = item.breastSize : accum = accum, 0);
-    const howFarAwayIs = Math.abs((topOneSize / playersArr[playerIndex].breastSize).toFixed(1));
+    const howFarAwayIs = Math.abs((topOneSize / playersArr[playerIndex]?.breastSize).toFixed(1)) || 1;
 
     try{
         ctx.deleteMessage();
@@ -212,6 +228,8 @@ bot.command('macat_siski', async (ctx) => {
             username: ctx.message.from.username,
             breastSize: size,
             lastPlayedTime: dateNow,
+            id: ctx.from.id,
+            name: ctx.from.first_name
         });
         ctx.reply(`Добре пiжалавать в игру друг (@${ctx.from.username}), твои сиськи выросли на ${size} cм, зиг хайль!`)
     }
@@ -256,10 +274,10 @@ bot.command('post_meme', async (ctx) => {
     }catch (e){
         console.log('delete error - ', ctx.from.username, ' - ', ctx.message.text)
     }
-    const regexp = /https:\/\/.+/;
-    const message = ctx.message.text.match(regexp)[0];
     
     try{
+        const regexp = /https:\/\/.+/;
+        const message = ctx.message.text.match(regexp)[0];
         const response = await fetch(`https://api.imgbb.com/1/upload?key=19df7fadd3a92f9e74543ebb91637b87&image=${message}`)
 
         const data = await response.json();
