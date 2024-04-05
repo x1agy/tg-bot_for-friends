4c3c38ddc734f9b5dd1f244bdbd03763c60cb83b
Alan <aapaev@zuzex.com>
2024-04-05 10:54:06 +0300

    fix: fixed quote

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { Telegraf, Input } from 'telegraf'
+import { Telegraf, Input } from 'telegraf';
 import fs from 'fs';
 import { createCanvas, loadImage } from 'canvas';
 import puppeteer from 'puppeteer';
@@ -20,11 +20,11 @@ function writePlayersData(data) {
     fs.writeFileSync('./playersData.json', JSON.stringify(data));
 }
 
-function writegameMessagesData(data){
+function writegameMessagesData(data) {
     fs.writeFileSync('./mesagesArray.json', JSON.stringify(data));
 }
 
-function readgameMessagesData(){
+function readgameMessagesData() {
     const data = fs.readFileSync('./mesagesArray.json');
     return JSON.parse(data);
 }
@@ -46,43 +46,58 @@ bot.on('text', async (ctx, next) => {
     const message = ctx.message;
     const gameMessages = readgameMessagesData();
 
-    if(message.text === '1488'){
-        ctx.reply('卐卐卐卐卐卐卐卐卐卐卐')
+    if (message.text === '1488') {
+        ctx.reply('卐卐卐卐卐卐卐卐卐卐卐');
     }
 
-    if(message.from.is_bot){
-        console.log(message.text)
+    if (message.from.is_bot) {
+        console.log(message.text);
     }
-    if(message.text === 'Игра начинается!'){
-        writegameMessagesData([{isGameStarted: true}])
+    if (message.text === 'Игра начинается!') {
+        writegameMessagesData([{ isGameStarted: true }]);
     }
-    if(!message.from.is_bot && gameMessages[0].isGameStarted){        
-        gameMessages.push({text: message.text, author: message.from.first_name + ' ' + message.from?.last_name});
+    if (!message.from.is_bot && gameMessages[0].isGameStarted) {
+        gameMessages.push({
+            text: message.text,
+            author: message.from.first_name + ' ' + message.from?.last_name,
+        });
         writegameMessagesData(gameMessages);
     }
-    if(message.text.search('Игра окончена!') !== -1){
+    if (message.text.search('Игра окончена!') !== -1) {
         const randomArray = [];
-        for(let i = 0; i < 3; i++){
+        for (let i = 0; i < 3; i++) {
             const randomNumber = Math.round(Math.random() * gameMessages.length);
-            randomArray.push(randomNumber !== 0 ? randomNumber : 1)
+            randomArray.push(randomNumber !== 0 ? randomNumber : 1);
         }
-        ctx.reply('лучший момент матча: ')
-        randomArray.map(item => gameMessages[item] ? ctx.reply(`${gameMessages[item]?.text} -- ${gameMessages[item]?.author}`) : false)
-        writegameMessagesData([{isGameStarted: false}]);
+        ctx.reply('лучший момент матча: ');
+        randomArray.map((item) =>
+            gameMessages[item]
+                ? ctx.reply(
+                    `${gameMessages[item]?.text} -- ${gameMessages[item]?.author}`
+                )
+                : false
+        );
+        writegameMessagesData([{ isGameStarted: false }]);
     }
 
-    if(ctx.message.text.toLowerCase().search('алан') !== -1){
+    if (ctx.message.text.toLowerCase().search('алан') !== -1) {
         console.log(message.text, '  ----  ', message.from.first_name);
-    }  
+    }
 
-    if(message.reply_to_message && message.text.toLowerCase() === 'помацать'){
-        ctx.reply(`${message.from.first_name} помацал(-а) сиськи ${message.reply_to_message.from.first_name}, +10% к удаче для не(-е, -го)!`)
+    if (message.reply_to_message && message.text.toLowerCase() === 'помацать') {
+        ctx.reply(
+            `${message.from.first_name} помацал(-а) сиськи ${message.reply_to_message.from.first_name}, +10% к удаче для не(-е, -го)!`
+        );
     }
-    if(message.text.toLowerCase() === 'секс'){
-        ctx.replyWithAnimation('https://media1.tenor.com/m/Dr8YBE5s5AEAAAAC/sex-alarm.gif')
+    if (message.text.toLowerCase() === 'секс') {
+        ctx.replyWithAnimation(
+            'https://media1.tenor.com/m/Dr8YBE5s5AEAAAAC/sex-alarm.gif'
+        );
     }
-    if(message.text.toLowerCase().split('').slice(0,10).join('') === 'бля ответь'){
-        ctx.reply('ИДИ НАХУЙ')
+    if (
+        message.text.toLowerCase().split('').slice(0, 10).join('') === 'бля ответь'
+    ) {
+        ctx.reply('ИДИ НАХУЙ');
     }
     //     const fixedMessage = message.text.replace('бля ответь', '');
     //     console.log(fixedMessage)
@@ -100,7 +115,7 @@ bot.on('text', async (ctx, next) => {
     //         ],
     //         stream: false,
     //     });
-        
+
     //     const requestConfig = {
     //         method: 'POST',
     //         headers: {
@@ -113,7 +128,7 @@ bot.on('text', async (ctx, next) => {
     //     fetch('https://api.pawan.krd/v1/chat/completions', requestConfig)
     //         .then(response => response.json())
     //         .then(data => {
-    //             // ctx.reply(data.choices[0].message.content)   
+    //             // ctx.reply(data.choices[0].message.content)
     //             console.log('data error - ', data)
     //         })
     //         .catch(e => {
@@ -122,38 +137,44 @@ bot.on('text', async (ctx, next) => {
     //         })
     // }
 
-    if(message.text.toLowerCase() === 'бля процитируй'){
-        if(ctx.message.reply_to_message && ctx.message.reply_to_message.text){
+    if (message.text.toLowerCase() === 'бля процитируй') {
+        if (ctx.message.reply_to_message && ctx.message.reply_to_message.text) {
             const repliedToUserId = ctx.message.reply_to_message.from.id;
-            bot.telegram.getUserProfilePhotos(repliedToUserId).then((userProfilePhotos) => {
+            bot.telegram
+                .getUserProfilePhotos(repliedToUserId)
+                .then((userProfilePhotos) => {
+                    if (userProfilePhotos.total_count > 0) {
+                        const photo = userProfilePhotos.photos[0][0];
 
-                if(userProfilePhotos.total_count > 0){
-                    const photo = userProfilePhotos.photos[0][0];
+                        bot.telegram.getFile(photo.file_id).then(async (fileInfo) => {
+                            const photoUrl = `https://api.telegram.org/file/bot${'6125258679:AAGGenW1GA67CIIcsfHhpRyznx5xfHrqcP0'}/${fileInfo.file_path}`;
 
-                    bot.telegram.getFile(photo.file_id).then(async (fileInfo) => {
-                        const photoUrl = `https://api.telegram.org/file/bot${'6125258679:AAGGenW1GA67CIIcsfHhpRyznx5xfHrqcP0'}/${fileInfo.file_path}`;
-                
-                        const replyFrom = ctx.message.reply_to_message.from
-                        const imageBuffer = await createImageWithTextAndPhoto(photoUrl, ctx.message.reply_to_message.text, replyFrom.first_name + ' ' + (replyFrom.last_name?.length ? replyFrom.last_name : ''));
+                            const replyFrom = ctx.message.reply_to_message.from;
+                            const imageBuffer = await createImageWithTextAndPhoto(
+                                photoUrl,
+                                ctx.message.reply_to_message.text,
+                                replyFrom.first_name +
+                                ' ' +
+                                (replyFrom.last_name?.length ? replyFrom.last_name : '')
+                            );
 
-                        const imageUrl = await postImageInImgBB(imageBuffer)
-                        ctx.replyWithPhoto(imageUrl.data.url);
-                    });
-                }else{
-                    ctx.reply('иди нахуй или открой мне аватарки')
-                }
-            })
-        }
-        else{
-            ctx.reply('иди нахуй')
+                            const imageUrl = await postImageInImgBB(imageBuffer);
+                            ctx.replyWithPhoto(imageUrl.data.url);
+                        });
+                    } else {
+                        ctx.reply('иди нахуй или открой мне аватарки');
+                    }
+                });
+        } else {
+            ctx.reply('иди нахуй');
         }
     }
 
-    if(message.text.toLowerCase() === 'бля праздники'){
-        try{
+    if (message.text.toLowerCase() === 'бля праздники') {
+        try {
             const browser = await puppeteer.launch();
             const page = await browser.newPage();
-            await page.goto('https://kakoysegodnyaprazdnik.ru/')
+            await page.goto('https://kakoysegodnyaprazdnik.ru/');
 
             await page.waitForTimeout(3000);
 
@@ -161,141 +182,175 @@ bot.on('text', async (ctx, next) => {
             await submitButton.click();
 
             await page.waitForSelector('span[itemprop="text"]');
-            const datesList = await page.$$eval('span[itemprop="text"]', spans => {
-                return spans.map(span => '• ' + span.textContent).join('\n');
+            const datesList = await page.$$eval('span[itemprop="text"]', (spans) => {
+                return spans.map((span) => '• ' + span.textContent).join('\n');
             });
 
             ctx.reply(datesList);
-            }catch(e){
-                console.log(e)
+        } catch (e) {
+            console.log(e);
         }
     }
 
-    if(message.text.toLowerCase().includes('бля айпи')){
+    if (message.text.toLowerCase().includes('бля айпи')) {
         const regexp = /\d+\.\d+\.\d+\.\d+/;
         const ip = message.text.match(regexp);
-        const response = fetch(``)
+        const response = fetch(``);
     }
 
-    next()
-})
+    next();
+});
 
 bot.command('macat_siski', async (ctx) => {
-
     const playersArr = readPlayersData();
 
-    const playerIndex = playersArr.findIndex(item => item.id === ctx.message.from.id);
+    const playerIndex = playersArr.findIndex(
+        (item) => item.id === ctx.message.from.id
+    );
     const size = Math.trunc(Math.random() * 100);
     const dateNow = new Date().getTime();
 
-    const topOneSize = playersArr.reduce((accum, item) => accum <= item.breastSize ? accum = item.breastSize : accum = accum, 0);
-    const howFarAwayIs = Math.abs((topOneSize / playersArr[playerIndex]?.breastSize).toFixed(1)) || 1;
+    const topOneSize = playersArr.reduce(
+        (accum, item) =>
+            accum <= item.breastSize ? (accum = item.breastSize) : (accum = accum),
+        0
+    );
+    const howFarAwayIs =
+        Math.abs((topOneSize / playersArr[playerIndex]?.breastSize).toFixed(1)) ||
+        1;
 
-    try{
+    try {
         ctx.deleteMessage();
-    }catch (e){
-        console.log('delete error - ', ctx.from.username, ' - ', ctx.message.text)
+    } catch (e) {
+        console.log('delete error - ', ctx.from.username, ' - ', ctx.message.text);
     }
 
-    if(playerIndex !== -1){
+    if (playerIndex !== -1) {
         const expiredTime = dateNow - playersArr[playerIndex].lastPlayedTime;
-        if(expiredTime < 14400000){
-            ctx.reply(`Друг (@${ctx.from.username}), не хуейте, вы сможете сыграть через ${14400000 - expiredTime}мс или ${Math.trunc((14400000 - expiredTime) / 60000)} минут`);
-        }else{
-            if(Math.trunc(Math.random() * 10) !== 0){
-                if(howFarAwayIs > 1.5){
-                    const bonusSize = Math.abs(Math.trunc((playersArr[playerIndex].breastSize / 100) * 7 * howFarAwayIs)); 
-                    ctx.reply(`Вы успешно дотянулись до своих tits @${ctx.from.username} и помацали их, +${size}см и +${bonusSize} за отставание, теперь ваши сиськи размером ${playersArr[playerIndex].breastSize + size + bonusSize}см, поздравляю друг`);
-                    
+        if (expiredTime < 14400000) {
+            ctx.reply(
+                `Друг (@${ctx.from.username}), не хуейте, вы сможете сыграть через ${14400000 - expiredTime
+                }мс или ${Math.trunc((14400000 - expiredTime) / 60000)} минут`
+            );
+        } else {
+            if (Math.trunc(Math.random() * 10) !== 0) {
+                if (howFarAwayIs > 1.5) {
+                    const bonusSize = Math.abs(
+                        Math.trunc(
+                            (playersArr[playerIndex].breastSize / 100) * 7 * howFarAwayIs
+                        )
+                    );
+                    ctx.reply(
+                        `Вы успешно дотянулись до своих tits @${ctx.from.username
+                        } и помацали их, +${size}см и +${bonusSize} за отставание, теперь ваши сиськи размером ${playersArr[playerIndex].breastSize + size + bonusSize
+                        }см, поздравляю друг`
+                    );
+
                     playersArr[playerIndex].breastSize += size + bonusSize;
                     playersArr[playerIndex].lastPlayedTime = dateNow;
-                }else{
-                    ctx.reply(`Вы успешно дотянулись до своих tits @${ctx.from.username} и помацали их, +${size}см, теперь ваши сиськи размером ${playersArr[playerIndex].breastSize + size}см, поздравляю друг`);
-                
+                } else {
+                    ctx.reply(
+                        `Вы успешно дотянулись до своих tits @${ctx.from.username
+                        } и помацали их, +${size}см, теперь ваши сиськи размером ${playersArr[playerIndex].breastSize + size
+                        }см, поздравляю друг`
+                    );
+
                     playersArr[playerIndex].breastSize += size;
                     playersArr[playerIndex].lastPlayedTime = dateNow;
                 }
-            }else{
-                ctx.reply(`Вы немножко проебались @${ctx.from.username}, -${size}см, теперь ваши сиськи размером ${playersArr[playerIndex].breastSize - size}см, пиздуйте`);
+            } else {
+                ctx.reply(
+                    `Вы немножко проебались @${ctx.from.username
+                    }, -${size}см, теперь ваши сиськи размером ${playersArr[playerIndex].breastSize - size
+                    }см, пиздуйте`
+                );
                 playersArr[playerIndex].breastSize -= size;
                 playersArr[playerIndex].lastPlayedTime = dateNow;
-                
             }
         }
-    }
-    else{
+    } else {
         playersArr.push({
             username: ctx.message.from.username,
             breastSize: size,
             lastPlayedTime: dateNow,
             id: ctx.from.id,
-            name: ctx.from.first_name
+            name: ctx.from.first_name,
         });
-        ctx.reply(`Добре пiжалавать в игру друг (@${ctx.from.username}), твои сиськи выросли на ${size} cм, зиг хайль!`)
+        ctx.reply(
+            `Добре пiжалавать в игру друг (@${ctx.from.username}), твои сиськи выросли на ${size} cм, зиг хайль!`
+        );
     }
-    writePlayersData(playersArr)
-})
-
-bot.command('all', async (ctx) => {
-    ctx.reply('ты проебался -500рублей')
+    writePlayersData(playersArr);
 });
 
+bot.command('all', async (ctx) => {
+    ctx.reply('ты проебался -500рублей');
+});
 
 const memesDB = readMemesData();
 
 bot.command('meme', async (ctx) => {
-
-    try{
+    try {
         ctx.deleteMessage();
-    }catch (e){
-        console.log('delete error - ', ctx.from.username, ' - ', ctx.message.text)
+    } catch (e) {
+        console.log('delete error - ', ctx.from.username, ' - ', ctx.message.text);
     }
-    ctx.replyWithPhoto(Input.fromURL(memesDB[Math.trunc(Math.random() * memesDB.length)]))
-})
+    ctx.replyWithPhoto(
+        Input.fromURL(memesDB[Math.trunc(Math.random() * memesDB.length)])
+    );
+});
 
 bot.command('top', async (ctx) => {
-
     const rawArr = readPlayersData();
     let laderString = '';
     const playersArr = rawArr.sort((a, b) => b.breastSize - a.breastSize);
-    playersArr.map((item, index) => laderString += `${index + 1}. ${item.name}, ${item.breastSize}см\n`);
-    ctx.reply(`Топ чувих с самыми огромными сиськами:\n${laderString} Если ваш ник помечен как undefined, прожмите /macat_siski`)
-    try{
+    playersArr.map(
+        (item, index) =>
+            (laderString += `${index + 1}. ${item.name}, ${item.breastSize}см\n`)
+    );
+    ctx.reply(
+        `Топ чувих с самыми огромными сиськами:\n${laderString} Если ваш ник помечен как undefined, прожмите /macat_siski`
+    );
+    try {
         ctx.deleteMessage();
-    }catch (e){
-        console.log('delete error - ', ctx.from.username, ' - ', ctx.message.text)
+    } catch (e) {
+        console.log('delete error - ', ctx.from.username, ' - ', ctx.message.text);
     }
-})
+});
 
 bot.command('post_meme', async (ctx) => {
-
-    try{
+    try {
         ctx.deleteMessage();
-    }catch (e){
-        console.log('delete error - ', ctx.from.username, ' - ', ctx.message.text)
+    } catch (e) {
+        console.log('delete error - ', ctx.from.username, ' - ', ctx.message.text);
     }
-    
-    try{
+
+    try {
         const regexp = /https:\/\/.+/;
         const message = ctx.message.text.match(regexp)[0];
-        const response = await fetch(`https://api.imgbb.com/1/upload?key=19df7fadd3a92f9e74543ebb91637b87&image=${message}`)
+        const response = await fetch(
+            `https://api.imgbb.com/1/upload?key=19df7fadd3a92f9e74543ebb91637b87&image=${message}`
+        );
 
         const data = await response.json();
 
-        if(data?.error){
-            ctx.reply(`Чё за хуйню ты мне скинул, друг ${data.error.message}`)
-        }else{
-            ctx.reply('Твой нацистский мем успешно загружен, наверное, не ебу, я не могу все ошибки обработать')
+        if (data?.error) {
+            ctx.reply(`Чё за хуйню ты мне скинул, друг ${data.error.message}`);
+        } else {
+            ctx.reply(
+                'Твой нацистский мем успешно загружен, наверное, не ебу, я не могу все ошибки обработать'
+            );
             memesDB.push(data.data.url);
             writeMemesData(memesDB);
         }
-    }catch (e) {
-        ctx.reply(`Друг, проебался ты со своим мемом. ${e}`)
+    } catch (e) {
+        ctx.reply(`Друг, проебался ты со своим мемом. ${e}`);
     }
-})
+});
 
 async function createImageWithTextAndPhoto(photoUrl, text, quotedNickname) {
-    const canvas = createCanvas(400, 220); 
+    const canvas = createCanvas(500, Math.trunc((text.length / 50) * 16) + 120);
+    console.log((text.length / 50) * 16)
     const ctx = canvas.getContext('2d');
 
     const photo = await loadImage(photoUrl);
@@ -305,16 +360,16 @@ async function createImageWithTextAndPhoto(photoUrl, text, quotedNickname) {
     ctx.arc(60, 60, 40, 0, Math.PI * 2);
     ctx.closePath();
     ctx.clip();
-    ctx.drawImage(photo, 20, 20, 80, 80); 
+    ctx.drawImage(photo, 20, 20, 80, 80);
     ctx.restore();
 
     ctx.font = 'bold 16px Arial';
     ctx.fillStyle = 'black';
-    ctx.fillText(quotedNickname, 120, 30);
+    ctx.fillText(quotedNickname, 120, 30, 480);
 
     ctx.font = '16px Arial';
     ctx.fillStyle = 'black';
-    wrapText(ctx, text, 120, 50, 260, 16);
+    wrapText(ctx, text, 120, 50, 460, 16);
 
     const imageBuffer = canvas.toBuffer('image/png');
 
@@ -340,7 +395,6 @@ function wrapText(context, text, x, y, maxWidth, lineHeight) {
     context.fillText(line, x, y);
 }
 
-
 async function postImageInImgBB(imgBuffer) {
     try {
         const imgBlob = new Blob([imgBuffer], { type: 'image/png' });
@@ -353,13 +407,13 @@ async function postImageInImgBB(imgBuffer) {
         });
 
         const data = await response.json();
-        return data
+        return data;
     } catch (e) {
         console.error('Error posting image in ImgBB', e);
     }
 }
 
-bot.launch()
+bot.launch();
 
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
