d17b13913ec4b7ce65f86903a25d84dd14a105c1
x1agy <skizzzgrand@gmail.com>
2024-05-21 22:55:18 +0300

    fix: added .env file and port var in it for deploy

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bot = new Telegraf('6125258679:AAGGenW1GA67CIIcsfHhpRyznx5xfHrqcP0')
 
 const AIApiKey = 'pk-yZxnfaHgquwVEZiaWtHJqfpYRlLZjVVWtzvKAvOdLWXQXMfa'
 
-const port = 10000
+const port = process.env.PORT || 10000
 
 function readPlayersData() {
 	try {
